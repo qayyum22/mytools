b32d5233518719bc669f7db37104f72d2cbac032
Qayyum <qayyumabdul660@gmail.com>
2024-06-30 21:39:46 +0530

    tailwind to css

--- src/pages/TailwindToCSS.jsx
+++ src/pages/TailwindToCSS.jsx
@@ -1,33 +1,86 @@
-import React from "react";
+import { useEffect } from "react";
 import { useState } from "react";
+import { convertFromCssToJss, getConvertedClasses } from "../data/Cheatsheet";
+import CopyToClipboard from "react-copy-to-clipboard";
+import { ClipboardIcon } from "@heroicons/react/24/outline";
+import { toast } from "sonner";
 
-const TailwindToCSS = () => {
 
-    const [tailwindcss, setTailwindcss] = useState(null);
 
-    const handleChange = () => {
-        setTailwindcss(e.target.value);
-    }
 
-    const handleClick = async () => {
-        await fetch('/', {
 
-        })
-    }
+
+
+
+
+
+
+export default function TailwindToCSS() {
+
+    const [input, setInput] = useState('');
+    const [result, setResult] = useState('');
+    const [resultJSS, setResultJSS] = useState('');
+
+    useEffect(() => {
+        const resultCss = getConvertedClasses(input);
+        const resultJSS = convertFromCssToJss(resultCss);
+        setResult(resultCss);
+        setResultJSS(resultJSS);
+    }, [input]);
+
 
 
     return (
-        <div className="page-container">
-            <h1>Tailwind CSS to CSS classes</h1>
-            <input type="text"
-                value={tailwindcss}
-                placeholder="input tailwind css code here"
-                onChange={handleChange} />
-            <button onClick={handleClick}>
-                Generate CSS Classes
-            </button>
-        </div>
-    )
-}
+        <main>
+            <nav>
+                <header>
+                    <h1>
+                        <div>
+                            <img src={logo} alt="Tailwind to CSS" />
+                        </div>
+                        <span>Tailwind to CSS</span>
+                    </h1>
 
-export default TailwindToCSS;
+                    <section>
+                        {/* github button */}
+                        <button>
+                            <a href="www.sblab.com">
+                                <span>sponsored by</span>
+                                <img src={sblogo} alt="sblab logo" />
+                                <span>SB Computer Labs</span>
+                            </a>
+                        </button>
+                    </section>
+                </header>
+            </nav>
+
+            <section>
+                <textarea name="input">
+                    enter tailwind class names
+                </textarea>
+                <CopyToClipboard text={result} onCopy={() => toast.success('Copied!')}>
+                    <ClipboardIcon className="h-6 w-6 text-gray-500" />
+                </CopyToClipboard>
+
+                <div>
+                    <textarea name="CSS">CSS</textarea>
+                    {/* copy to clipboard button */}
+                </div>
+
+
+
+                <div>
+                    <textarea name="JSS (beta)">
+                        resultJSS
+                    </textarea>
+                    <CopyToClipboard text={resultJSS} onCopy={() => toast.success('Copied!')}>
+                        <ClipboardCopyIcon className="w-6 h-6 mt-3 text-gray-500 cursor-pointer md:mr-1" />
+                    </CopyToClipboard>
+                </div>
+
+
+            </section>
+        </main>
+    )
+
+}
